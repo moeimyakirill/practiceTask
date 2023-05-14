3224064ca1d25b396371ac057b8b7f35440d2f68
moeimyakirill <110389316+moeimyakirill@users.noreply.github.com>
2023-05-14 14:41:55 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Задание 1
+# n = int(1)
+# for i in range(100):
+#     if n % 3 == 0 and n % 5 == 0:
+#         print(f'FizzBuzz {n}')
+#     elif n % 3 == 0:
+#         print(f'Fizz {n}')
+#     elif n % 5 == 0:
+#         print(f'Buzz {n}')
+#     else:
+#         print(n)
+#     n += 1
